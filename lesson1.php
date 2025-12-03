3e6d8d678d88f364ed3249c200b8ad539414aaee
Muhammad <a45538983@gmail.com>
2025-12-03 04:37:53 +0300

    First commit

---
+++ lesson1.php
@@ -0,0 +1,34 @@
+<?php
+    //nachalo php
+    echo "hello world";
+    $mycounter = 1;
+    $mystring = "hello world";
+    $myarray = array(1, 2, 3, 4, 5);
+    $userName = "Fred Smith";
+    $current_user = $userName;
+    echo "<br>";
+    //Chislovye peremennye
+    echo $current_user;
+    $count = 17;
+    $count = 7.5;
+    //Array
+    $team = array("Bill","Mick","Mary","Chris","Ann");
+    echo "<br>";
+    echo $team[2];
+
+    $oxo = array(array("x"," ","o"),
+                  array("o","x"," "),
+                  array(" ","o","x"));
+    echo "<br>";
+    echo $oxo[1][1];
+
+    echo "<br>";
+    //operatory
+    echo 6+2;
+    $count = 0;
+    $count +=1;
+    echo "<br>";
+    echo $count;
+
+    
+    ?>
